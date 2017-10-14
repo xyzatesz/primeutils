5c0c046290ecce3f19d87b5bf2ed0827fb3aa821
xyzatesz <30017192+xyzatesz@users.noreply.github.com>
2017-10-14 16:39:49 +0200

    Update version number

--- autobuild.sh
+++ autobuild.sh
@@ -9,13 +9,16 @@ cargo build --release --target=i686-pc-windows-gnu
 echo Success: cargo build --release --target=platform
 
 cp ./target/release/primeutils ./bin/primeutils-x86_64-unknown-linux-gnu
-cp ./target/i686-unknown-linux-gnu/release/primeutils ./bin/primeutils-i686-unknown-linux-gnu
-cp ./target/x86_64-pc-windows-gnu/release/primeutils.exe ./bin/primeutils-x86_64-pc-windows-gnu.exe
-cp ./target/i686-pc-windows-gnu/release/primeutils.exe ./bin/primeutils-i686-pc-windows-gnu.exe
-echo Success: cp ./target/platform/executable ./bin/executable-platform
-
 strip ./bin/primeutils-x86_64-unknown-linux-gnu
+
+cp ./target/i686-unknown-linux-gnu/release/primeutils ./bin/primeutils-i686-unknown-linux-gnu
 strip ./bin/primeutils-i686-unknown-linux-gnu
+
+cp ./target/x86_64-pc-windows-gnu/release/primeutils.exe ./bin/primeutils-x86_64-pc-windows-gnu.exe
 strip ./bin/primeutils-x86_64-pc-windows-gnu.exe
+
+cp ./target/i686-pc-windows-gnu/release/primeutils.exe ./bin/primeutils-i686-pc-windows-gnu.exe
 strip ./bin/primeutils-i686-pc-windows-gnu.exe
+
+echo Success: cp ./target/platform/executable ./bin/executable-platform
 echo Success: strip ./bin/executable-platform
